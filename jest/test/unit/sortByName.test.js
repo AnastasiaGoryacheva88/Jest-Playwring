71cecf246cd123421b023e017b9b78afc0e0b892
Anastasia test <gorandi@inbox.ru>
2023-07-22 18:15:05 +0300

    first task

--- jest/test/unit/sortByName.test.js
+++ jest/test/unit/sortByName.test.js
@@ -2,16 +2,26 @@ const sorting = require("../../app");
 
 describe("Books names test suit", () => {
   it("Books names should be sorted in ascending order", () => {
-    expect(
-      sorting.sortByName([
-        "Гарри Поттер",
-        "Властелин Колец",
-        "Волшебник изумрудного города",
-      ])
-    ).toEqual([
-      "Властелин Колец",
-      "Волшебник изумрудного города",
-      "Гарри Поттер",
-    ]);
+    const input = ["Гарри Поттер",
+    "Властелин Колец",
+    "Волшебник изумрудного города",
+
+    ]
+    const expected = [
+    "Властелин Колец",
+    "Волшебник изумрудного города",
+    "Гарри Поттер",
+  ]
+  const result = sorting.sortByName(input)
+
+  expect(result).toEqual(expected);
   });
+
+  test ("Function throws exception when called whitout parms (TypeError)", () =>{
+expect(() => sorting.sortByName()).toThrow(TypeError)
+  })
+  test ("Function throws exception when called whitout parms", () =>{
+    expect(() => sorting.sortByName()).toThrow()
+      })
 });
+
