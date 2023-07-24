8c59b9de77cc441190b468b596a33c945f7ebd6b
Anastasia test <gorandi@inbox.ru>
2023-07-24 20:34:49 +0300

    second task

--- playwright/tests/App.spec.js
+++ playwright/tests/App.spec.js
@@ -5,7 +5,7 @@ test("test", async ({ page }) => {
   await page.goto("https://netology.ru/free/management#/");
 
   // Click a
-  await page.click("a");
+  await page.click("a"),{force: true };
   await expect(page).toHaveURL("https://netology.ru/");
 
   // Click text=Учиться бесплатно
